00500adfcb1cb205400e1dce591350d8d348a2e9
Kota Watanabe <kota3316@gmail.com>
2019-06-26 16:37:03 -0700

    create operator.js

---
+++ operator.js
@@ -0,0 +1,8 @@
+function solve(meal_cost, tip_percent, tax_percent) {
+    const tip = meal_cost *(tip_percent/100);
+    const tax = meal_cost *(tax_percent/100);
+    return Math.round(meal_cost + tip + tax);
+}
+
+
+console.log(solve(12.00,20,8)) //15
